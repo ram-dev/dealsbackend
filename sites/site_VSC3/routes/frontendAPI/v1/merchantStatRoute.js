525743bc5f8b62260062542baaea72c0d73289a2
chintalaravi teja <chintalaravi.teja@kohls.com>
2018-01-30 17:31:40 +0530

    stats updated

--- sites/site_VSC3/routes/frontendAPI/v1/merchantStatRoute.js
+++ sites/site_VSC3/routes/frontendAPI/v1/merchantStatRoute.js
@@ -17,18 +17,13 @@ const USER_RESPONSE = {
 };
 
 module.exports.use = function(Router) {    
-    Router.get('/v1/merchantstat/:merchantId', getMerchantStat());
+    Router.get('/v1/merchantstat', getMerchantStat());
 };
 
 function getMerchantStat() {
     return [
-        passport.isBearerAndMerchantOrMerchantAdminOrSuperAdmin,
-        getmerchantID,
-        CheckUserAccess,
-        config,
-        queryPageing,
-        queryRestrictionForMerchant,
-        queryRestrictToFetchMerchant,
+        passport.isBearerAndMerchantOrMerchantAdminOrSuperAdmin,               
+        config,        
         getDeals,
         getDownloads,
         FetchMechants,
@@ -37,57 +32,24 @@ function getMerchantStat() {
 }
 
 function config(req, res, next) {
+    var user = req.user;
     req.yoz.result = {
         total_balance : 0,
         total_deals : 0,
         total_downloads : 0,
         active_deals: 0,
         inactive_deals :0
-    }
+    }    
     req.yoz.query = {};
+    req.yoz.query._id = user.merchant
     req.yoz.condition = {};
     next();
 };
 
-function queryPageing(req, res, next) {
-    if (req.query.skip != undefined) {
-        req.yoz.condition.skip = Number(req.query.skip);
-    }
-    if (req.query.limit != undefined) {
-        req.yoz.condition.limit = Number(req.query.limit);
-    }
-    if (req.query.sortBy != undefined) {
-        req.yoz.condition.sort = req.query.sortBy;
-    }
-
-    next();
-};
-
-function queryRestrictToFetchMerchant(req, res, next) {
-    var userInfoId = req.params.merchantId;
-    if (userInfoId === undefined) {
-        return next();
-    }
-    req.yoz.query._id = userInfoId;
-    next();
-};
-
-function FetchMechants(req, res, next) {
-    var MerchantModel = req.yoz.db.model(dbModels.merchantModel);
-    var query = req.yoz.query;   
-    MerchantModel.find(query, {}, req.yoz.condition).
-    exec(function(err, user) {
-        if (err) {
-            return restHelper.unexpectedError(res, err);
-        }        
-        req.yoz.userInfoObj = user;
-        next();
-    });
-};
-
 function getDeals(req, res, next) {
+    var user = req.user;
     var DealModel = req.yoz.db.model(dbModels.dealModel);
-    var query = { merchantId : req.params.merchantId};   
+    var query = { merchantId : user.merchant};   
     DealModel.find(query, {}, req.yoz.condition).
     exec(function(err, deal) {
         if (err) {
@@ -99,14 +61,28 @@ function getDeals(req, res, next) {
 };
 
 function getDownloads(req, res, next) {
+    var user = req.user;
     var DownloadDeal = req.yoz.db.model(dbModels.downloadDealModel);
-    var query = { merchantId : req.params.merchantId};   
+    var query = { merchantId : user.merchant};   
     DownloadDeal.find(query, {}, req.yoz.condition).
     exec(function(err, download) {
         if (err) {
             return restHelper.unexpectedError(res, err);
         }        
         req.yoz.downloadDealObj = download;        
+        next();
+    });
+};
+
+function FetchMechants(req, res, next) {
+    var MerchantModel = req.yoz.db.model(dbModels.merchantModel);
+    var query = req.yoz.query;   
+    MerchantModel.find(query, {}, req.yoz.condition).
+    exec(function(err, user) {
+        if (err) {
+            return restHelper.unexpectedError(res, err);
+        }        
+        req.yoz.userInfoObj = user;
         next();
     });
 };
@@ -130,66 +106,4 @@ function format(req, res) {
     req.yoz.result.active_deals = activeDeals.length;
     req.yoz.result.inactive_deals = inactiveDeals.length;   
     restHelper.OK(res, req.yoz.result);
-
 };
-
-function queryRestrictionForMerchant(req, res, next) {
-    var user = req.user;
-    var Types = req.yoz.db.Schemas[dbModels.roleModel].Types;
-
-    if (user.roleId.equals(Types.SuperAdminRole)) {
-        return next();
-    } else {
-        req.yoz.query.userId = user._id;
-    }
-    next();
-};
-
-function getmerchantID(req, res, next) {    
-    var user = req.user;   
-    var Types = req.yoz.db.Schemas[dbModels.roleModel].Types;
-    if(req.params.merchantId == undefined){
-        if (user.roleId.equals(Types.MerchantRole)) {
-            req.params.merchantId = user.merchant;
-            next()
-        }else{
-            next()
-        }
-    }else{
-        next()
-    }     
-};
-
-function CheckUserAccess(req, res, next) {    
-    var user = req.user;   
-    var Types = req.yoz.db.Schemas[dbModels.roleModel].Types;
-
-    if (user.roleId.equals(Types.SuperAdminRole) || user.roleId.equals(Types.MerchantAdminRole)) {
-        return next();
-    } else if (user.roleId.equals(Types.MerchantRole)) {
-
-        var merchantId = req.params.merchantId;
-        if (user.merchant.equals(merchantId)) {
-            return next();
-        }
-        return restHelper.accessDenied(res);
-    } else {
-        return restHelper.unAuthorized(res, USER_RESPONSE.UPDATE_CLINIC_UNAUTHORIZED);
-    }
-};
-
-function addParameters(dbObj, restObj) {
-    var result = {};
-    for (var property in restObj) {
-        if (restObj[property] instanceof Object) {
-            result[property] = addParameters(dbObj[property], restObj[property]);
-        }
-        else {
-            result[property] = dbObj[property];
-            dbObj[property] = restObj[property];
-        }
-    }
-    return result;
-};
-
-
